59cd5413c5f2aba26706e2a79d5bb56e99fc589c
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-05-31 18:01:58 -0300

    First commit

---
+++ spring-boot-asynchronous-service/src/main/java/com/matheuscordeiro/springbootasynchronousservice/SpringBootAsynchronousServiceApplication.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro.springbootasynchronousservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAsynchronousServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAsynchronousServiceApplication.class, args);
+	}
+
+}
---
+++ spring-boot-asynchronous-service/src/test/java/com/matheuscordeiro/springbootasynchronousservice/SpringBootAsynchronousServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro.springbootasynchronousservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAsynchronousServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
