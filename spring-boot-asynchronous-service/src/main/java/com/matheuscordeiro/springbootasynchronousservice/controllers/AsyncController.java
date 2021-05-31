b3aea21bb7920fa7e6000e129c201cf8c632e8d9
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-05-31 18:34:50 -0300

    inject service in controller

--- spring-boot-asynchronous-service/src/main/java/com/matheuscordeiro/springbootasynchronousservice/controllers/AsyncController.java
+++ spring-boot-asynchronous-service/src/main/java/com/matheuscordeiro/springbootasynchronousservice/controllers/AsyncController.java
@@ -1,16 +1,37 @@
 package com.matheuscordeiro.springbootasynchronousservice.controllers;
 
+import com.matheuscordeiro.springbootasynchronousservice.models.EmployeeAddresses;
+import com.matheuscordeiro.springbootasynchronousservice.models.EmployeeNames;
+import com.matheuscordeiro.springbootasynchronousservice.models.EmployeePhone;
+import com.matheuscordeiro.springbootasynchronousservice.services.AsyncService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 @RestController
 public class AsyncController {
     private static Logger LOG = LoggerFactory.getLogger(AsyncController.class);
 
+    @Autowired
+    AsyncService service;
+
     @GetMapping("async")
-    public void getAsync() {
+    public void getAsync() throws InterruptedException, ExecutionException {
         LOG.info("get Async start");
+
+        CompletableFuture<EmployeeAddresses> employeeAddress = service.getEmployeeAddress();
+        CompletableFuture<EmployeeNames> employeeName = service.getEmployeeName();
+        CompletableFuture<EmployeePhone> employeePhone = service.getEmployeePhone();
+
+        CompletableFuture.allOf(employeeAddress, employeeName, employeePhone);
+
+        LOG.info("EmployeeAddress--> " + employeeAddress.get());
+        LOG.info("EmployeeName--> " + employeeName.get());
+        LOG.info("EmployeePhone--> " + employeePhone.get());
     }
 }
